565711ffaf1ff859080a2fcdf3cb23dacfc6d673
Jacob Reola <jacobreola@gmail.com>
2024-09-19 11:55:00 -0500

    difference

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+
+// function Greeting() {
+//   return <h2>My First Component</h2>;
+// }
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(<Greeting />);
 
 function Greeting() {
-  return <h2>My First Component</h2>;
+  return (
+    <>
+      <div className="someValue">
+        <h3>hello people</h3>
+        <ul>
+          <li>
+            <a href="#">hello world</a>
+          </li>
+        </ul>
+      </div>
+      <h2>hello world</h2>
+      <input type="text" name="" id="" />
+    </>
+  );
 }
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(<Greeting />);
